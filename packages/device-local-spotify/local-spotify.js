81b51453f15ead21b4dcd7e1f2b5f1087002f8c5
Matt Gaunt <matt@gauntface.co.uk>
2017-01-16 15:21:53 -0800

    Should make the local spotify a little more reliable

--- packages/device-local-spotify/local-spotify.js
+++ packages/device-local-spotify/local-spotify.js
@@ -13,49 +13,75 @@ class LocalSpotify extends RouteDevice {
     if (!id) {
       id = 'LocalSpotify';
     }
+
     super(id);
 
-    this._init();
+    this._initMopidy();
   }
 
-  _init() {
-    if (this._ready) {
-      return;
+  _initMopidy() {
+    this._mopidyOnline = false;
+    this._mopidyInstance = new Mopidy({
+      callingConvention: 'by-position-or-by-name',
+      webSocketUrl: 'ws://localhost:6680/mopidy/ws/',
+      console: {
+        log: () => {},
+        warn: () => {},
+        error: () => {},
+        info: () => {},
+        debug: () => {},
+      },
+    });
+
+    let lastEventName = null;
+    this._mopidyInstance.on((eventName) => {
+      switch(eventName) {
+        case 'websocket:incomingMessage':
+        case 'websocket:outgoingMessage':
+          return;
+        default:
+          if (lastEventName !== eventName) {
+            lastEventName = eventName;
+            this.logHelper.log(`Mopidy: ${eventName}`);
+          }
+          break;
+      }
+    });
+    this._mopidyInstance.on('state:online', () => {
+      this._mopidyOnline = true;
+    });
+    this._mopidyInstance.on('state:offline', () => {
+      this._mopidyOnline = false;
+    });
+  }
+
+  _getMopidyInstance() {
+    this._spawnMopidyService();
+
+    if (this._mopidyOnline) {
+      return Promise.resolve(this._mopidyInstance);
     }
 
-    this._ready = new Promise((resolve) => {
-      this._spawnMopidyService();
-
-      const mopidy = new Mopidy({
-        callingConvention: 'by-position-or-by-name',
-        webSocketUrl: 'ws://localhost:6680/mopidy/ws/',
-        console: {
-          log: () => {},
-          warn: () => {},
-          error: () => {},
-          info: () => {},
-          debug: () => {},
-        },
-      });
-
-      mopidy.on((eventName) => {
-        switch(eventName) {
-          case 'websocket:incomingMessage':
-          case 'websocket:outgoingMessage':
-            return;
-          default:
-            /* eslint-disable no-console */
-            this.logHelper.log(`Mopidy: ${eventName}`);
-            /* eslint-enable no-console */
-            break;
-        }
-      });
-      mopidy.on('state:online', () => resolve(mopidy));
+    return new Promise((resolve) => {
+      const listener = () => {
+        this._mopidyInstance.off('state:online', listener);
+        resolve(this._mopidyInstance);
+      };
+      this._mopidyInstance.on('state:online', listener);
     });
   }
 
   _spawnMopidyService() {
+    if (this._mopidyService) {
+      return;
+    }
+
     this._mopidyService = spawn('mopidy');
+    this._mopidyService.on('close', (code, signal) => {
+      this.logHelper.log('Mopidy Service is closed.');
+
+      this._mopidyService = null;
+    });
   }
 
   playPlaylist(playlistUrl, shouldShuffle = true) {
@@ -65,7 +91,7 @@ class LocalSpotify extends RouteDevice {
         `hostname '${SPOTIFY_HOSTNAME}' instead found: '${playlistUrl.host}'`));
     }
 
-    return this._ready.then((mopidyInstance) => {
+    return this._getMopidyInstance().then((mopidyInstance) => {
       const mopidyUri = `spotify${playlistUrl.path.replace(/\//g, ':')}`;
       return mopidyInstance.playlists.filter({uri: mopidyUri})
       .then((playlists) => {
@@ -95,18 +121,28 @@ class LocalSpotify extends RouteDevice {
   }
 
   stop() {
-    return this._ready.then((mopidyInstance) => {
-      return mopidyInstance.playback.stop()
-      .then(() => {
-        if (this._mopidyService) {
-          this._mopidyService.kill();
-        }
-      });
+    return this._getMopidyInstance().then((mopidyInstance) => {
+      return mopidyInstance.playback.stop();
     });
   }
 
   exit() {
-    return this.stop();
+    // Close the WebSocket without reconnecting.
+    this._mopidyInstance.close();
+    // Unregister all event listeners.
+    this._mopidyInstance.off();
+    // Delete your reference to the object, so it can be garbage collected.
+    this._mopidyInstance = null;
+
+    // Kill mopidy service.
+    if (this._mopidyService) {
+      return new Promise((resolve) => {
+        this._mopidyService.on('close', resolve);
+        spawn('killall', ['mopidy']);
+      });
+    }
+
+    return Promise.resolve();
   }
 }
 
--- packages/device-local-spotify/test/device-local-spotify.js
+++ packages/device-local-spotify/test/device-local-spotify.js
@@ -1,39 +1,39 @@
+const spawnSync = require('child_process').spawnSync;
 const LocalSpotify = require('../local-spotify');
 
 describe('Test Local Spotify Device', function() {
   const PLAYLIST_EXAMPLE = 'https://play.spotify.com/user/mattgaunt/playlist/6pOIdTUvKFiMmzlG4clupX';
 
   it('should be constructed without values', function() {
-    new LocalSpotify();
+    this.timeout(6 * 1000);
+
+    const spotifyDevice = new LocalSpotify();
+    spotifyDevice.exit();
   });
 
-  it('should be able to play and stop playlist after initialisation', function() {
-    this.timeout(10 * 1000);
+  it('should not throw when stopping an non-playing track', function() {
+    this.timeout(15 * 1000);
+
+    const spotifyDevice = new LocalSpotify();
+    return spotifyDevice.stop()
+    .then(() => {
+      return spotifyDevice.exit();
+    });
+  });
+
+  it('should be able to play, stop, play and stop playlist after initialisation', function() {
+    this.timeout(30 * 1000);
 
     const spotifyDevice = new LocalSpotify();
     return spotifyDevice.playPlaylist(PLAYLIST_EXAMPLE)
     .then(() => {
       return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 2000);
+        setTimeout(resolve, 2000);
       });
     })
     .then(() => {
       return spotifyDevice.stop();
-    });
-  });
-
-  it('should not throw when stopping an non-playing track', function() {
-    const spotifyDevice = new LocalSpotify();
-    return spotifyDevice.stop();
-  });
-
-  it('should be able to play, play and stop playlist after initialisation', function() {
-    this.timeout(10 * 1000);
-
-    const spotifyDevice = new LocalSpotify();
-    return spotifyDevice.playPlaylist(PLAYLIST_EXAMPLE)
+    })
     .then(() => {
       return new Promise((resolve) => {
         setTimeout(resolve, 2000);
@@ -49,6 +49,72 @@ describe('Test Local Spotify Device', function() {
     })
     .then(() => {
       return spotifyDevice.stop();
+    })
+    .then(() => {
+      return spotifyDevice.exit();
+    });
+  });
+
+  it('should handle killing of mopidy service', function() {
+    this.timeout(40 * 1000);
+
+    const spotifyDevice = new LocalSpotify();
+    return spotifyDevice.playPlaylist(PLAYLIST_EXAMPLE)
+    .then(() => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyDevice._mopidyService.on('close', () => resolve());
+        spawnSync('killall', ['mopidy']);
+      });
+    })
+    .then(() => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+    })
+    .then(() => {
+      return spotifyDevice.playPlaylist(PLAYLIST_EXAMPLE);
+    })
+    .then(() => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+    })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        spotifyDevice._mopidyService.on('close', () => resolve());
+        spawnSync('killall', ['mopidy']);
+      });
+    })
+    .then(() => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+    })
+    .then(() => {
+      return spotifyDevice.stop();
+    })
+    .then(() => {
+      return spotifyDevice.exit();
+    });
+  });
+
+  it('should be able to exit mopidy', function() {
+    this.timeout(20 * 1000);
+
+    const spotifyDevice = new LocalSpotify();
+    return spotifyDevice.playPlaylist(PLAYLIST_EXAMPLE)
+    .then(() => {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+    })
+    .then(() => {
+      return spotifyDevice.exit();
     });
   });
 });
